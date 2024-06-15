7fb31e0fb76057f0e43dff93d9ada9c764b26c13
Varun Behere <varunbehere@gmail.com>
2024-06-15 13:13:48 +0530

    18th Done

---
+++ 18/Code1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void Display(int iNo1, int iNo2){
+  int i = 0, j = 0;
+  for (i = 1; i<= iNo1; i++){
+    for (j = 1; j<= iNo2; j++){
+      if ( j == (iNo2-i)+1) printf("#\t");
+      else printf("*\t");
+    }
+    printf("\n" );
+  }
+}
+
+int main (){
+  int iValue1 = 0, iValue2 = 0;
+  printf("Rows: ");
+  scanf("%d", &iValue1);
+  printf("Cols: ");
+  scanf("%d", &iValue2);
+
+  Display(iValue1, iValue2);
+
+  return 0;
+}
---
+++ 18/Code4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void Display(int iNo1, int iNo2){
+  int i = 0, j = 0;
+  for (i = 1; i<= iNo1; i++){
+    for (j = 1; j<= iNo2; j++){
+      if ( j == (iNo2-i)+1 || i == 1 || j == 1 || i == iNo1 || j == iNo2 ) printf("*\t");
+      else {
+        if (j > (iNo2-i)+1)  printf("$\t");
+        if (j < (iNo2-i)+1)  printf("#\t");
+      }
+
+    }
+    printf("\n" );
+  }
+}
+
+int main (){
+  int iValue1 = 0, iValue2 = 0;
+  printf("Rows: ");
+  scanf("%d", &iValue1);
+  printf("Cols: ");
+  scanf("%d", &iValue2);
+
+  Display(iValue1, iValue2);
+
+  return 0;
+}
